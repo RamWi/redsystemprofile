536dd2455ade3d37d16f10161704d24addae575e
RamWi <agungrama931@gmail.com>
2025-10-29 15:29:13 +0800

    konfigurasi react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-red-600'>Build Here</h1>
+    </>
+  )
+}
+
+export default App
